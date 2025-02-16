f608f9f2972a963d360f546d9ab817fa7eab1d28
HamzaMuhd <hamzamuhdidris@gmail.com>
2025-02-16 17:25:31 +0100

    feat: Implement authorization checks for protected routes in authentication

--- auth.ts
+++ auth.ts
@@ -104,8 +104,21 @@ export const config = {
       }
       return token;
     },
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     authorized({ request, auth }): any {
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order/,
+        /\/admin/,
+      ];
+
+      const { pathname } = request.nextUrl;
+
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
       if (!request.cookies.get("sessionCartId")) {
         const sessionCartId = crypto.randomUUID();
 
