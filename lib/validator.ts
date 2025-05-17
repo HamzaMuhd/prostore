b86866fdc424826d1c399b49ff90273921a7708e
HamzaMuhd <hamzamuhdidris@gmail.com>
2025-05-17 11:50:34 +0100

    feat: add review functionality with schema, validation, and default values

--- lib/validator.ts
+++ lib/validator.ts
@@ -128,3 +128,16 @@ export const updateUserScheme = updateProfileSchema.extend({
   id: z.string().min(1, "ID is required"),
   role: z.string().min(1, "Role is required"),
 });
+
+//Schema to insert reviews
+export const insertReviewSchema = z.object({
+  title: z.string().min(3, "Title must be atleast 3 character s "),
+  description: z.string().min(3, "Description must be atleast 3 character s "),
+  productId: z.string().min(1, "Product is required"),
+  userId: z.string().min(1, "User is required"),
+  rating: z.coerce
+    .number()
+    .int()
+    .min(1, "Rating must be at least 1")
+    .max(5, "Rating must be at most 5"),
+});
