2be8937abcb5f5864ff5cc397b627e368e9b45a2
HamzaMuhd <hamzamuhdidris@gmail.com>
2025-05-20 10:17:03 +0100

    fix: reorder import statements for consistency in email module

--- email/index.tsx
+++ email/index.tsx
@@ -1,8 +1,8 @@
 import { Resend } from "resend";
 import { SENDER_EMAIL, APP_NAME } from "@/lib/constants";
 import { Order } from "@/types";
-import dotenv from "dotenv";
 import PurchaseReceiptEmail from "./purchase-receipt";
+import dotenv from "dotenv";
 dotenv.config();
 
 const resend = new Resend(process.env.RESEND_API_KEY as string);
